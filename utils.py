86d5c7d5f34bcad6537d40258c5b93981272d9e2
Roheet <roheetbasu@gmail.com>
2026-06-05 11:15:00 +0545

    added function that adds random back ground

--- utils.py
+++ utils.py
@@ -4,3 +4,12 @@ def set_seed(seed = None):
     if seed is not None:
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+        
+
+def add_random_background(image, bg_color=None):
+    bg_color = np.random.rand() * 255 if bg_color is None else bg_color
+    image = np.array(image)
+    rgb, alpha = image[..., :3], image[..., 3:]
+    alpha = alpha.astype(np.float32) / 255.0
+    image_new = rgb * alpha + bg_color * (1 - alpha)
+    return Image.fromarray(image_new.astype(np.uint8))
