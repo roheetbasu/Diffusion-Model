21170beb32486ce69ee204c50e233e133f642c9a
Roheet <roheetbasu@gmail.com>
2026-06-05 11:04:37 +0545

    added config details

---
+++ config.py
@@ -0,0 +1,11 @@
+class config:
+    
+    seed = 1234
+    num_frames = 7
+    mode = "pixel"
+    offset_noise = True
+    camera_views = [1,2,3,4,5,0,0]
+    resize_rate = 1.0
+    random_background = False
+    ref_position = 6
+    
